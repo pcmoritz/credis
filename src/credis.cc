5b83400322e9a3a769df61da64891badcf660fc4
Philipp Moritz <pcmoritz@gmail.com>
2017-11-17 23:15:03 -0800

    interface with Consul

--- src/credis.cc
+++ src/credis.cc
@@ -1,9 +1,14 @@
+#include <iostream>
+
+#include <cpr/cpr.h>
+
 #include "redismodule.h"
 
 int ChainInitialize_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   REDISMODULE_NOT_USED(ctx);
   REDISMODULE_NOT_USED(argc);
   REDISMODULE_NOT_USED(argv);
+
   return REDISMODULE_OK;
 }
 
@@ -15,6 +20,12 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
   if (RedisModule_Init(ctx,"credis",1,REDISMODULE_APIVER_1)
       == REDISMODULE_ERR)
     return REDISMODULE_ERR;
+
+  std::cout << "Initializing 'credis' module:" << std::endl;
+
+  auto r = cpr::Get(cpr::Url{"localhost:8500/v1/catalog/nodes"},
+                    cpr::Header{{"accept", "application/json"}});
+  std::cout << "Consul nodes: " << r.text << std::endl;
 
   if (RedisModule_CreateCommand(ctx,"credis.initialize",
       ChainInitialize_RedisCommand,"write",1,1,1) == REDISMODULE_ERR)
