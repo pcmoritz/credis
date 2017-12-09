edd1f6f65fa74416994650e64360cbbf1cbef956
Zongheng Yang <zongheng.y@gmail.com>
2017-12-09 10:57:55 -0800

    Fix propagation (max-update sn), cleanups, TAIL.CHECKPOINT, MEMBER.SN

---
+++ init.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -ex
+
+pushd build
+make clean
+make -j
+popd
+
+# Head, Middle, Tail.
+./redis/src/redis-server --loadmodule ./build/src/libmember.so --port 6379 &> head.log &
+./redis/src/redis-server --loadmodule ./build/src/libmember.so --port 6380 &> middle.log &
+./redis/src/redis-server --loadmodule ./build/src/libmember.so --port 6381 &> tail.log &
+
+# Master.
+./redis/src/redis-server --loadmodule ./build/src/libmaster.so --port 6369 &> master.log &
+sleep 3
+redis-cli -p 6369 MASTER.ADD 127.0.0.1 6379
+redis-cli -p 6369 MASTER.ADD 127.0.0.1 6380
+redis-cli -p 6369 MASTER.ADD 127.0.0.1 6381
+
+# Tests below
+echo
+
+redis-cli -p 6379 MEMBER.SN
+redis-cli -p 6380 MEMBER.SN
+redis-cli -p 6381 MEMBER.SN
+echo
+
+# To test TAIL.CHECKPOINT
+sleep 3
+redis-cli -p 6379 member.put a 1
+# (nil)
+redis-cli -p 6381 get a
+# "1"
+redis-cli -p 6381 tail.checkpoint
+# (integer) 1
+redis-cli -p 6381 tail.checkpoint
+# (integer) 0
+redis-cli -p 6379 member.put a 2
+# (nil)
+redis-cli -p 6379 member.put fasfas asd
+# (nil)
+redis-cli -p 6381 tail.checkpoint
+# (integer) 2
+echo
+
+# To test MEMBER.SN
+redis-cli -p 6379 MEMBER.SN
+redis-cli -p 6380 MEMBER.SN
+redis-cli -p 6381 MEMBER.SN
